e10cd37b2370fb32abe6f815c4df68a52cd35d0b
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-02-13 11:18:08 +0100

    dokoncenie tyzdenneho projektu

--- tyzdenny_projekt_1.py
+++ tyzdenny_projekt_1.py
@@ -1,8 +1,11 @@
 retazec = input("?")
+
 dlzka = len(retazec)
 print("dlzka =", dlzka)
+
 prevrat = retazec[::-1]
 print("prevrat =", prevrat)
+
 for riadok in range(dlzka):
     for pismeno in retazec:
         print(f"{pismeno} * ", end="")
