b83c6a29b52c29ee5a8ef5b54176b5d9ceeff6dd
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-03-14 22:04:05 +0100

    Update tyzdenny_projekt_1.py

--- tyzdenny_projekt_1.py
+++ tyzdenny_projekt_1.py
@@ -1,12 +1,12 @@
 retazec = input("?")
 
-dlzka = len(retazec)
-print("dlzka =", dlzka)
+dlzka_retazca = len(retazec)
+print("dlzka =", dlzka_retazca)
 
-prevrat = retazec[::-1]
-print("prevrat =", prevrat)
+iny_retazec = retazec[::-1]
+print("prevrat =", iny_retazec)
 
-for riadok in range(dlzka):
+for riadok in range(dlzka_retazca):
     for pismeno in retazec:
         print(f"{pismeno} * ", end="")
     print()
