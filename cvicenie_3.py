0820c1ff1fe9d3057f0449bd08c6c56fff1f635d
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-03-16 08:44:45 +0100

    Update cvicenie_3.py

--- cvicenie_3.py
+++ cvicenie_3.py
@@ -213,7 +213,18 @@ for i in range(1, n+1):
     x1, y1 = x2, y2
 
 # 19)
-# skusim to dokoncit
+canvas = tk.Canvas()
+canvas.pack()
+n = int(input("zadaj n: "))
+x, y, r = 180, 130, 125
+uhol = 360 / n
+for i in range(n):
+    for j in range(i+1, n):
+        x1 = x + r * cos(radians(uhol*i))
+        y1 = y + r * sin(radians(uhol*i))
+        x2 = x + r * cos(radians(uhol*j))
+        y2 = y + r * sin(radians(uhol*j))
+        canvas.create_line(x1, y1, x2, y2)
 
 # 20)
 # skusim to dokoncit
